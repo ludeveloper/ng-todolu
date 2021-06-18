e7f54e9bdf3910e0cd085ce7cd14ed9b97ab8c62
Lutfullah Ugurer <LuDeveloper@Lus-MacBook.local>
2021-06-18 19:49:18 +0300

    added angular lazy loading, added angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent,
-    AboutComponent
   ],
   imports: [
     BrowserModule,
